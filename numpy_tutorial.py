0206a2b775189a8de3159dd353a3ebcbadcbb27d
Sonu_roy_29 <163444998+Sonuroy29@users.noreply.github.com>
2026-04-16 22:06:26 +0530

    Add comment for clarity on multidimensional arrays
    Added a comment to clarify the use of multidimensional arrays.

--- numpy_tutorial.py
+++ numpy_tutorial.py
@@ -4,6 +4,7 @@ array = np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']],
                   [['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R']],
                   [['S', 'T', 'U'], ['V', 'W', 'X'], ['Y', 'Z', ' ']]])
 
+# using multidimensional arrays :-
 word = array[2, 0, 0] + array[1, 1, 2] + array[1, 1, 1] + array[2, 0, 2] + array[2, 2, 2] + array[1, 2, 2] + array[1, 1, 2] + array[2, 2, 0]
 
 print(word)
